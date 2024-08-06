88d20f793d767e376551e70fc3da6b668b2e5857
naruse-ke <keisuke.naruse@rakus-partners.co.jp>
2024-08-06 14:47:25 +0900

    ログイン画面実装 ・AdministratorController.java ・LoginForm.java ・login.html

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
+import com.example.form.LoginForm;
 import com.example.service.AdministratorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,17 @@ public class AdministratorController {
 
     @Autowired
     private AdministratorService administratorService;
+
+    /**
+     * ログイン画面の表示.
+     * 
+     * @param form LoginForm
+     * @return ログイン画面
+     */
+    @GetMapping("/")
+    public String toLogin(LoginForm form) {
+        return "administrator/login";
+    }
 
     /**
      * 管理者登録画面の表示.
@@ -35,7 +47,7 @@ public class AdministratorController {
      * 管理者情報を登録する.
      * 
      * @param form InsertAdministratorForm
-     * @return ログイン画面
+     * @return ログイン画面へリダイレクト
      */
     @PostMapping("/insert")
     public String insert(InsertAdministratorForm form) {
