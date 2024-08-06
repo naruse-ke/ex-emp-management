cbef32b1579e6127c720227239653f1edab5e130
naruse-ke <keisuke.naruse@rakus-partners.co.jp>
2024-08-06 14:14:02 +0900

    管理者登録処理の実装

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,11 +1,14 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * 管理者登録画⾯を表⽰する処理を実装する.
@@ -28,4 +31,17 @@ public class AdministratorController {
         return "administrator/insert";
     }
 
+    /**
+     * 管理者情報を登録する.
+     * 
+     * @param form InsertAdministratorForm
+     * @return ログイン画面
+     */
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form) {
+        Administrator administrator = new Administrator();
+        BeanUtils.copyProperties(form, administrator);
+        administratorService.insert(administrator);
+        return "redirect:/";
+    }
 }
