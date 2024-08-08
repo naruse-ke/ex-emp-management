05e98f74e2a03742b33fdc2bef3d8f26c29b689f
naruse-ke <keisuke.naruse@rakus-partners.co.jp>
2024-08-08 09:36:11 +0900

    従業員⼀覧を表⽰する機能の作成 - EmployeeControllerの作成 - UpdateEmployeeFormの作成 - EmployeeServiceの作成 - list.htmlの作成

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,28 @@
+package com.example.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+
+/**
+ * 従業員情報についての業務処理を実装する.
+ */
+@Service
+@Transactional
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    /**
+     * 従業員情報を全件取得する.
+     * 
+     * @return List Employee 全ての従業員情報
+     */
+    public List<Employee> showList() {
+        return employeeRepository.findAll();
+    }
+}
