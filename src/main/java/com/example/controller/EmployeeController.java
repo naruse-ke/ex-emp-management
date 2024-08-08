d40e28216d4527d7a4a120c85ee237435a4535ed
naruse-ke <keisuke.naruse@rakus-partners.co.jp>
2024-08-08 11:35:52 +0900

    扶養人数を更新する機能の実装 - EmployeeControllerに更新メソッド追加 - EmployeeServiceに更新メソッド追加 - detail.htmlの修正

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.domain.Employee;
 import com.example.form.UpdateEmployeeForm;
 import com.example.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * 従業員情報を検索する処理を記述する.
@@ -42,5 +44,19 @@ public class EmployeeController {
     public String showDetail(String id, Model model, UpdateEmployeeForm form) {
         model.addAttribute("employee", employeeService.showDetail(Integer.parseInt(id)));
         return "employee/detail.html";
+    }
+
+    /**
+     * 従業員詳細を更新する.
+     * 
+     * @param form UpdateEmployeeForm
+     * @return 従業員⼀覧画面へリダイレクト
+     */
+    @PostMapping("/update")
+    public String update(UpdateEmployeeForm form) {
+        Employee employee = employeeService.showDetail(Integer.parseInt(form.getId()));
+        employee.setDependentsCount(Integer.parseInt(form.getDependentsCount()));
+        employeeService.update(employee);
+        return "redirect:/employee/showList";
     }
 }
